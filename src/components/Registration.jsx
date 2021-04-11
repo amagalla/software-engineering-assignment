c020093e549513111f7da0c85948fe09df23f0a6
amagalla <anthonymagallanes24@gmail.com>
2021-04-10 17:23:51 -0700

    Registration form built out

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,7 +1,32 @@
+import "../styles/registration.scss";
+
 const Registration = () => {
   return (
-    <div className='registration-container'>
-      <p>Registration</p>
+    <div className='registration-main-container'>
+      <div className='registration-container'>
+        <h1>Registration</h1>
+        <form>
+          <div>
+            <p>First Name</p>
+            <input type='text' />
+            <p>Address</p>
+            <input type='text' />
+            <p>City</p>
+            <input type='text' />
+            <p>Country</p>
+            <input type='text' />
+          </div>
+          <div>
+            <p>Last Name</p>
+            <input type='text' />
+            <p>Address 2 (Optional)</p>
+            <input type='text' />
+            <p>State</p>
+            <input type='text' />
+            <input type='submit' />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
